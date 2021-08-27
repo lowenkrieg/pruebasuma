05c5c8f93ddeabd4034ab7f140f17ef1e82657ce
Eduardo Leon <eduardoleon@air-de-eduardo.home>
2021-08-26 21:01:16 -0300

    suma

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+function sumar(valor1, valor2) {
+    return valor1 + valor2;
+}
+console.log(sumar(10, 5));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function sumar(valor1:number, valor2:number): number {
+    return valor1+valor2;
+  }
+  
+  console.log(sumar(10, 5));
