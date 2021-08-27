f562de4bb974364f0254d3d736ab2cd5e3b66883
Eduardo Leon <eduardoleonguerra@gmail.com>
2021-08-27 07:39:57 -0300

    nuveoco

--- index.js
+++ index.js
@@ -2,4 +2,16 @@
 function sumar(valor1, valor2) {
     return valor1 + valor2;
 }
-console.log(sumar(10, 5));
+function multiplicar(valor3, valor4) {
+    return valor3 * valor4;
+}
+function restar(valor1, valor3) {
+    return valor1 - valor3;
+}
+function preciofinal(pb, tasad, tasaiva) {
+    return pb + (tasad / 100) + (tasaiva / 100);
+}
+console.log(sumar(3, 5));
+console.log(multiplicar(3, 5));
+console.log(restar(3, 3));
+console.log(preciofinal(100, 28, 45));
--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
 function sumar(valor1:number, valor2:number): number {
     return valor1+valor2;
   }
+
+function multiplicar(valor3:number, valor4:number): number {
+return valor3*valor4;
+}
+function restar(valor1:number, valor3:number): number{
+  return valor1-valor3;
+}
+function preciofinal(pb:number, tasad:number, tasaiva: number): number{
+  return pb+(tasad/100)+(tasaiva/100);
+}
   
-  console.log(sumar(10, 5));
+
+  console.log(sumar(3, 5));
+  console.log(multiplicar(3,5));
+  console.log(restar(3,3));
+  console.log(preciofinal(100, 28, 45));
